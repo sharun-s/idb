f0d1562c3755a2bd871968e6dcfdd8e5f0569075
sharun <sharun.msgs@gmail.com>
2020-02-15 18:24:03 +0530

    handling honorifics/titles in names add 3 fns- striptitles, gettitles, getUnrecognizeNoTitle

--- imine-pandas.py
+++ imine-pandas.py
@@ -1,26 +1,33 @@
 import pandas as p
 from pandas.api.types import CategoricalDtype
+from titles import *
 #import readline
 #readline.write_history_file('/home/s/idb/idb-panda.log')
 
-f=open('dashboard-padmaawards_gov_in_get_data')
-l=f.read()
-f.close()
-l=eval(l)
+def loadfile(name, evaluate=False):
+    with open(name) as f:
+        if evaluate:
+            data=eval(f.read())
+        else:
+            data=f.read()
+    return data
 
-dash=p.DataFrame(l)
-dash=dash.drop('id', axis='columns')
+def createDataFrame(data):
+	dash=p.DataFrame(data)
+	dash=dash.drop('id', axis='columns')
+	dash['name']=dash['name'].str.strip()
+	dash['place']=dash['place'].str.strip()
+	acat=dash['award'].value_counts().index.tolist()
+	acat.reverse()
+	dash['award']=dash['award'].astype(CategoricalDtype(categories=acat, ordered=True))
+	dash.place=dash.place.str.strip()
+	#locs=CategoricalDtype(categories=dash.place.value_counts().index.tolist())
+	#dash.place=dash.place.astype(locs)
+	dash.area=dash.area.astype(CategoricalDtype(categories=dash.area.value_counts().index.tolist()))
+	return dash
 
-acat=dash['award'].value_counts().index.tolist()
-acat.reverse()
-dash['award']=dash['award'].astype(CategoricalDtype(categories=acat, ordered=True))
-
-dash.place=dash.place.str.strip()
-dash.place=dash.place.astype(CategoricalDtype(categories=dash.place.value_counts().index.tolist()))
-
-dash.area=dash.area.astype(CategoricalDtype(categories=dash.area.value_counts().index.tolist()))
-
-g=dash.groupby(['area','year'])
+#def cf():
+#g=dash.groupby(['area','year'])
 
 # 1 cum freq of each area over time
 #gd=g.describe()
@@ -30,7 +37,68 @@ g=dash.groupby(['area','year'])
 #g.agg({'award':lambda x:len(x)}).unstack().transpose().cumsum().ffill().plot()
 
 # 3 - same as 2 without g
-# dash.groupby(['year','area']).agg({'award':lambda x:len(x)}).unstack()['award'].cumsum().ffill().plot()
+#dash.groupby(['year','area']).agg({'award':lambda x:len(x)}).unstack()['award'].cumsum().ffill().plot()
+	#return g.award.value_counts().unstack().cumsum().ffill().plot()
+	# 4 using pivot_table - SLOW!!
+	# dash.pivot_table(index=['year'],columns=['place'],aggfunc=len)
+# 5 - groupby year - no need for area, year
+	# dash.groupby(['year']).place.value_counts().unstack().cumsum()[['Delhi','Maharashtra']].plot()
+	# dash.groupby(['year']).place.value_counts().unstack().cumsum()[lambda x:x.columns.difference(['Delhi','Maharashtra','Tamil Nadu','West Bengal','Karnatake'])].ffill()
 
-# 4 cumfreq of each area over time for a particular state - Tamil Nadu
-dash.groupby(['year','area']).place.value_counts().unstack()['Tamil Nadu'].unstack().cumsum().ffill().plot()
+# freq distribution
+def fd(prop='place',top=10):
+	return dash[prop].value_counts()[:top]
+
+# cumulative freq
+def cf(df=dash, x='year', y='place', include_only=['Delhi','Maharashtra','Tamil Nadu'], exclude=['Delhi','Maharashtra','Tamil Nadu','West Bengal','Karnatake']):
+	# 1 using groupby area year - unnecessary
+	#g.place.value_counts().unstack()[state].unstack().cumsum().ffill().plot()
+	if include_only:
+		return df.groupby(x)[y].value_counts().unstack().cumsum()[include_only]		
+	if exclude:
+		return df.groupby(x)[y].value_counts().unstack().cumsum()[lambda x:x.columns.difference(exclude)].ffill()
+
+def facet(df, prop='place',val="Tamil Nadu"):
+	return df[df[prop].str.contains(val)]
+
+def stripTitles(df):
+	return df['name'].str.replace(titles,'')
+
+def getTitles(df):
+	# str[1] is a bit of a hack cuz where no recognizable title exists array will contain the whole name eg [Prabhu Deva] doing a str[1] makes it Nan
+	return df['name'].str.split('('+gtitles+')').str[1]
+
+def getNamesWithUnrecognized_NO_Titles(df):
+	return df['name'].str.split('('+titles+')').str[0].value_counts()  
+
+l=loadfile('dashboard-padmaawards_gov_in_get_data', evaluate=True)
+dash=createDataFrame(l)
+
+alltitles=getTitles()
+alltitles.value_counts(normalize=True)
+alltitles.value_counts(dropna=False) # check NA found
+# find specific title
+filter('name','Gen.').sort_values('place')
+# women - 861
+women=filter('name', )
+
+# dash.place.value_counts()[:6]
+# Delhi            810
+# Maharashtra      786
+# Tamil Nadu       413
+# Uttar Pradesh    314
+# West Bengal      274
+# Karnataka        255
+# Name: place, dtype: int64
+# >>> dash.place.value_counts()[:6].sum()
+# 2852
+# >>> dash.place.value_counts().sum()
+# 4615
+# >>> 2852/4615
+# 0.6179848320693391
+# >>> dash.place.value_counts()[:10].sum()
+# 3421
+# >>> 2421/4615
+# 0.5245937161430119
+# >>> 3421/4615
+# 0.7412784398699892
--- titles.py
+++ titles.py
@@ -1,4 +1,144 @@
-titles='Air Vice Mar. |Br. |Dr. Rm. |Dr.\(Kum.\) |Dr. \(Kum.\) |Dr.\(Ms.\) |Dr.\(Mrs.\) |Dr. \(Mrs.\) |Dr. \(Ms.\) |Dr. \(Smt.\) |\(Dr.\) |Dr. |Dr.|Isai |Kumari |Kum. |Late Shri |Late Smt. |Late \(Smt\) |Maj. Gen. |Maj. |Gen. |Miss |Ms. |Ms |Nartaki |Prof.\(Dr\) |Prof. \(Dr.\) |Prof. |Prof |Sadhguru |Sheik |Shri |Vice Adm. |Adm. |\(Miss\) |\(Smt.\) |Smt. '
+# usualful when stripping title from name
+titles='Swami |Sardar |Sheik |Father |Fr. |Most Venerable |Her Royal Highness Princess |Hon. Commodore |Bishop |Nawab |Rear Admiral |Admiral |Prof\.\(Mrs\.\) |His Holiness |Congressman |Sir |Card. |Maulana |Air Com\. |Wing Cdr |Lt\.Gen\.\(Rt\.\) |Late\. \(Gen\.\) |His Excellency |Acharya |Vaidya |Vaidyan |Yogacharya |Muni |Lord |Maharaj |Kumar |Rajmata |Rajkumari |Rani |Hakim |Sant |Guru |Air Marshal |Air Mar\. |Air Vice Marshal |Air Vice Mar\. |Ar\.V\.Mar\. |Br\. |Dr\. Rm\. |Dr\.\(Kum.\) |Dr\. \(Kum.\) |Dr\.\(Ms.\) |Dr\.\(Mrs.\) |Dr\. \(Mrs\.\) |Dr\. \(Ms.\) |Dr\. \(Smt.\) |\(Dr\.\) |Dr\. |Dr\.|Isai |Kumari |Kum\. |Late Shri |Late Smt\. |Late \(Shri\) |Late \(Smt\) |Late |Major |Maj\. Gen\. |Maj\. |Lt\. |Gen\. |Nartaki |Prof\.\(Dr\) |Prof\. \(Dr.\) |Prof\. |Prof |Sadhguru |Sheik |Shri |Vice Adm\. |Adm\. |\(Miss\) |\(Smt\.\) |Smt\. |Smt |Mrs\. |Grp\.Capt\. |Hon\. Capt\. |Capt\. |Ustad |Begum |Begam |Sister |Rev\. |Rev |Pandit |Pandita |Pt\. |Justice |Mr\. |Col\. |Brig\.\(Retd\) |Brig\. |Late\. \(Gen\.\) Miss |Ms\. |Ms '
+
+#5-106 titles found
+#group them
+#figure out the order ^(?=.*Prof)(?=.*Smt)
+#optional prefix = (
+#optional suffix = ).
+
+# prefix='\('
+# suffix='[\.\)]'
+# lookahead=lambda x:"(?=.*{0})".format(x)
+# prefix_suffix_lookahead=lambda x:"(?=.*{1}?{0}{2}?)".format(x,prefix,suffix)
+# acad=["Prof","Dr","Smt","Mrs","Miss","Ms"]
+# def genlookaheadregex(tokenlist):
+# 	r=''
+# 	for i in tokenlist:
+# 		r=r+lookahead(i)
+# 	return r
+
+# title honorific salutation
+
+mil='''
+Hon. Commodore |
+Rear Admiral |
+Admiral |
+Air Com\. |
+Wing Cdr |
+Lt\.Gen\.\(Rt\.\) |
+Late\. \(Gen\.\) |
+Air Marshal |
+Air Mar\. |
+Air Vice Marshal |
+Air Vice Mar\. |
+Ar\.V\.Mar\. |
+Br\. |
+Major |
+Maj\. Gen\. |
+Maj\. |
+Lt\. |
+Gen\. |
+Vice Adm\. |
+Adm\. |
+Grp\.Capt\. |
+Hon\. Capt\. |
+Capt\. |
+Col\. |
+Brig\.\(Retd\) |
+Brig\. |
+Late\. \(Gen\.\) |
+'''
+
+groupedtitles='''
+Swami |
+Acharya |
+Vaidya |
+Vaidyan |
+Yogacharya |
+Muni |
+Sant |
+Guru |
+Sadhguru |
+Pandit |
+Pandita |
+Pt\. |
+
+His Holiness |
+Most Venerable |
+
+Justice |
+
+Sheik |
+Maulana |
+Hakim |
+Ustad |
+Begum |
+Begam |
+
+Sister |
+Rev\. |
+Rev |
+Father |
+Fr. |
+Card. |
+Bishop |
+
+Her Royal Highness Princess |
+Sardar |
+Nawab |
+Congressman |
+His Excellency |
+Lord |
+Lady |
+Sir |
+Maharaj |
+Kumar |
+Rajmata |
+Rajkumari |
+Rani |
+
+Prof\.\(Mrs\.\) |
+Dr\. Rm\. |
+Dr\.\(Kum.\) |
+Dr\. \(Kum.\) |
+Dr\.\(Ms.\) |
+Dr\.\(Mrs.\) |
+Dr\. \(Mrs\.\) |
+Dr\. \(Ms.\) |
+Dr\. \(Smt.\) |
+\(Dr\.\) |
+Dr\. |
+Dr\.|
+Prof\.\(Dr\) |
+Prof\. \(Dr.\) |
+Prof\. |
+Prof |
+
+Isai |
+Nartaki |
+
+Kumari |
+Kum\. |
+Late Shri |
+Late Smt\. |
+Late \(Shri\) |
+Late \(Smt\) |
+Late |
+Shri |
+\(Miss\) |
+\(Smt\.\) |
+Smt\. |
+Smt |
+Mrs\. |
+Mr\. |
+Miss |
+Ms\. |
+Ms '''
+
+
+# this adds () around each group in ops where its reqd to work out which title name has
+gtitles="|".join("({0})".format(i) for i in titles.split('|'))
 
 initial=lambda n: ' '.join([i[0]+'.' for i in n.split(' ')[:-1]]) +' '+ n.split(' ')[-1]
 tightinitial=lambda n: ''.join([i[0]+'.' for i in n.split(' ')[:-1]]) +' '+ n.split(' ')[-1]
