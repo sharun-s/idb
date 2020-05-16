2d98631d172c073ad25c316924a17890bec927c7
sharun <sharun.msgs@gmail.com>
2020-05-17 00:58:07 +0530

    minor refac

--- tnmap3d.py
+++ tnmap3d.py
@@ -10,53 +10,50 @@ from matplotlib.colors import LightSource
 # Get colormaps to use with lighting object.
 from matplotlib import cm
 
+def Poly(ax, geom, forecolor, bordercolor, borderwidth=1, transparency=1., zheight=0):
+	pp=PolygonPatch(geom, fc=forecolor, ec=bordercolor, lw=borderwidth,alpha=transparency, zorder=zheight )
+	xy=pp.get_window_extent()
+	ax.add_patch(pp)
+	art3d.pathpatch_2d_to_3d(pp, z=zheight, zdir="z")
+	return xy
+
 BLUE = '#6699cc'
 
 l1=gp.read_file('tn_boundary.json')
 l2=gp.read_file('tn_dist.json')
 
-#fig = p.figure(facecolor='white')
-fig = p.figure(facecolor="#001f3f") 
-ax = fig.gca(projection='3d')
-districts=l2[l2.Name.isin(['Coimbatore','Erode','Kanyakumari'])].geometry
 alld=l2.geometry
+districts=l2[l2.Name.isin(sys.argv[4:])].geometry
 tn=l1.iloc[0].geometry
 
-pp=PolygonPatch(tn, fc='#22aacc', ec=BLUE, alpha=0.3, zorder=0 )
-xy=pp.get_window_extent()
-ax.add_patch(pp)
-art3d.pathpatch_2d_to_3d(pp, z=0, zdir="z")
+fig = p.figure(facecolor="#001f3f") 
+ax = fig.gca(projection='3d')
+
+window=Poly(ax,geom=tn,forecolor='#22aacc', bordercolor=BLUE, transparency=0.3, zheight=0)
 
 for j in districts:
-	pbase=PolygonPatch(j, fc='yellow', ec='#002f4f', lw=1, zorder=2 )
-	ax.add_patch(pbase)
-	art3d.pathpatch_2d_to_3d(pbase, z=0, zdir="z")	
+	#base
+	Poly(ax,geom=j,forecolor='yellow', bordercolor='#002f4f', borderwidth=1, zheight=2)
+	#top
+	Poly(ax,geom=j,forecolor='#001f3f', bordercolor='yellow', borderwidth=0.5, zheight=2)
 
-	pp=PolygonPatch(j, fc='#001f3f', ec="yellow", alpha=1., lw=.5, zorder=2 )
-	ax.add_patch(pp)
-	art3d.pathpatch_2d_to_3d(pp, z=2, zdir="z")
-
-	zinc=0
-	end=6
-	for i in range(0,2*end):
-		pbase=PolygonPatch(j, fc='yellow', ec=BLUE, alpha=0.2, lw=0, zorder=1 )
-		ax.add_patch(pbase)
-		art3d.pathpatch_2d_to_3d(pbase, z=i/(1.0*end), zdir="z")	
+	#zinc=0
+	#end=6
+	#for i in range(0,2*end):
+	#	pbase=PolygonPatch(j, fc='yellow', ec=BLUE, alpha=0.2, lw=0, zorder=1 )
+	#	ax.add_patch(pbase)
+	#	art3d.pathpatch_2d_to_3d(pbase, z=i/(1.0*end), zdir="z")	
 
 for j in alld:
-	pp=PolygonPatch(j, fc='yellow', ec="#001f3f", alpha=0.1, lw=1, zorder=2 )
-	ax.add_patch(pp)
-	art3d.pathpatch_2d_to_3d(pp, z=2, zdir="z")
-
-
+	Poly(ax,geom=j,forecolor='yellow', bordercolor='#001f3f', transparency=0.2, borderwidth=1, zheight=2)
 
 # pp=PolygonPatch(tn, fc='#001f3f', ec='#ffff00', alpha=0.2, zorder=0 )
 # ax.add_patch(pp)
 # art3d.pathpatch_2d_to_3d(pp, z=2, zdir="z")
 
 #ax.set_top_view()
-ax.set_xlim3d(xy.xmin, xy.xmax) #8600000, 8800000)
-ax.set_ylim3d(xy.ymin, xy.ymax)#1360000, 1440000)
+ax.set_xlim3d(window.xmin, window.xmax) #8600000, 8800000)
+ax.set_ylim3d(window.ymin, window.ymax)#1360000, 1440000)
 ax.set_zlim3d(0, 4)
 
 #ax=l1.plot(facecolor='#0099dd',edgecolor='blue',label='TN',alpha=.78)
@@ -86,6 +83,6 @@ ax.set_facecolor('#001f3f')
 #                 linewidth=0,
 #                 zorder=10)
 
-p.show()
-#fig.savefig(sys.argv[1],format='png',facecolor=fig.get_facecolor())
+#p.show()
+fig.savefig(sys.argv[1],format='png',facecolor=fig.get_facecolor())
 
