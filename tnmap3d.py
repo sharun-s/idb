4bca2bc0ebc5a1d69f382bcfc3c4ed70a25c5551
sharun <sharun.msgs@gmail.com>
2020-05-30 23:50:45 +0530

    padma refactor

--- tnmap3d.py
+++ tnmap3d.py
@@ -6,13 +6,12 @@ from descartes import PolygonPatch
 import numpy as np
 import sys
 
-from matplotlib.colors import LightSource
-# Get colormaps to use with lighting object.
-from matplotlib import cm
+#1. Big gap between axis and edge of screen?
+#2. Setting xlim and ylim causes stretching
 
 def Poly(ax, geom, forecolor, bordercolor, borderwidth=1, transparency=1., zheight=0):
 	pp=PolygonPatch(geom, fc=forecolor, ec=bordercolor, lw=borderwidth,alpha=transparency, zorder=zheight )
-	xy=pp.get_window_extent()
+	xy=pp.get_extents()
 	ax.add_patch(pp)
 	art3d.pathpatch_2d_to_3d(pp, z=zheight, zdir="z")
 	return xy
@@ -25,47 +24,61 @@ l2=gp.read_file('tn_dist.json')
 alld=l2.geometry
 districts=l2[l2.Name.isin(sys.argv[4:])].geometry
 tn=l1.iloc[0].geometry
+tnx=tn.centroid.x
+tny=tn.centroid.y
 
-fig = p.figure(facecolor="#001f3f") 
+fig = p.figure(facecolor="#001f3f",figsize=(10.24,7.68)) 
+#fig.subplots_adjust(left=-0.4,right=1.4)
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 ax = fig.gca(projection='3d')
-
-window=Poly(ax,geom=tn,forecolor='#22aacc', bordercolor=BLUE, transparency=0.3, zheight=0)
-
-for j in districts:
-	#base
-	Poly(ax,geom=j,forecolor='yellow', bordercolor='#002f4f', borderwidth=1, zheight=2)
-	#top
-	Poly(ax,geom=j,forecolor='#001f3f', bordercolor='yellow', borderwidth=0.5, zheight=2)
-
-	#zinc=0
-	#end=6
-	#for i in range(0,2*end):
-	#	pbase=PolygonPatch(j, fc='yellow', ec=BLUE, alpha=0.2, lw=0, zorder=1 )
-	#	ax.add_patch(pbase)
-	#	art3d.pathpatch_2d_to_3d(pbase, z=i/(1.0*end), zdir="z")	
-
-for j in alld:
-	Poly(ax,geom=j,forecolor='yellow', bordercolor='#001f3f', transparency=0.2, borderwidth=1, zheight=2)
-
-# pp=PolygonPatch(tn, fc='#001f3f', ec='#ffff00', alpha=0.2, zorder=0 )
-# ax.add_patch(pp)
-# art3d.pathpatch_2d_to_3d(pp, z=2, zdir="z")
-
-#ax.set_top_view()
-#print(window.xmin, window.xmax,window.ymin, window.ymax)
-ax.set_xlim3d(window.xmin, window.xmax) #8600000, 8800000)
-ax.set_ylim3d(window.ymin, window.ymax)#1360000, 1440000)
-ax.set_zlim3d(0, 5)
-ax.set_aspect(.85)
-#ax=l1.plot(facecolor='#0099dd',edgecolor='blue',label='TN',alpha=.78)
 e=int(sys.argv[2])
 azi=int(sys.argv[3]) or -90.
 ax.set_axis_off()
+ax.set_aspect(0.73*1.33)
 ax.view_init(elev=e, azim=azi)
-ax.set_facecolor('#001f3f')
-#l2[l2.Name=='Tiruppur'].plot(ax=ax,facecolor='orange',edgecolor='blue',label='TN',alpha=.73, linewidth=1) 
-#p.ison()
 
+ax.set_facecolor('#001f3f')
+#ax.get_proj=lambda:np.dot(Axes3D.get_proj(ax),np.diag([1.04,1.15,1,1])) 
+
+window=Poly(ax,geom=tn,forecolor='#22aacc', bordercolor=BLUE, transparency=0.3, zheight=0)
+#ax.plot([tnx],[tny],[1],'go')
+print(window.xmin, window.xmax, window.xmax-window.xmin)
+print(window.ymin, window.ymax, window.ymax-window.ymin)
+print(10.24/7.68)
+print((window.xmax-window.xmin)/(window.ymax-window.ymin))
+ax.set_xlim3d(window.xmin-70000, window.xmax-150000) #8600000, 8800000)
+ax.set_ylim3d(window.ymin, window.ymax-150000)#1360000, 1440000)
+ax.set_zlim3d(0, 3)
+#ax.margins(1.1,.1,0.,tight=True)
+print(ax.get_proj())
+print(ax.get_w_lims())
+for j in districts:
+	#base
+	Poly(ax,geom=j,forecolor='yellow', bordercolor='#002f4f', borderwidth=1, zheight=2)
+	ax.plot([j.centroid.x, j.centroid.x],[j.centroid.y, j.centroid.y],[1,2],'y',linewidth=4)
+	#top
+	#Poly(ax,geom=j,forecolor='yellow', bordercolor='yellow', borderwidth=0.5, zheight=2)
+
+	# zinc=0
+	# end=6
+	# for i in range(0,2*end):
+	# 	pbase=PolygonPatch(j, fc='yellow', ec=BLUE, alpha=0.2, lw=0, zorder=1 )
+	# 	ax.add_patch(pbase)
+	# 	art3d.pathpatch_2d_to_3d(pbase, z=i/(1.0*end), zdir="z")	
+
+for j in alld:
+	Poly(ax,geom=j,forecolor='yellow', bordercolor='#001f3f', transparency=0.2, borderwidth=1, zheight=1)
+	#ax.plot([tnx, j.centroid.x],[tny, j.centroid.y],[1],'r')	
+
+bbox = fig.bbox_inches.from_bounds(2, 1, 8, 7)
+fig.savefig(sys.argv[1],format='png', bbox_inches=bbox, facecolor=fig.get_facecolor())
+
+
+from matplotlib.colors import LightSource
+# Get colormaps to use with lighting object.
+from matplotlib import cm
+
+# Use Lighting
 # y = np.linspace(xy.ymin, xy.ymax)
 # x = np.linspace(xy.xmin, xy.xmax)
 # yy, xx = np.meshgrid(y, x)
@@ -85,5 +98,4 @@ ax.set_facecolor('#001f3f')
 #                 zorder=10)
 
 #p.show()
-fig.savefig(sys.argv[1],format='png',facecolor=fig.get_facecolor())
 
