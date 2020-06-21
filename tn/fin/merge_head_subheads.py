df7f0586e8fa53d3d026384ad7b139ec9461cc03
sharun <sharun.msgs@gmail.com>
2020-06-21 21:07:16 +0530

    bugfix subgroups in rev details were getting swallowed up

--- tn/fin/merge_head_subheads.py
+++ tn/fin/merge_head_subheads.py
@@ -11,51 +11,108 @@ df=p.read_csv('rev_details.csv')
 len(df)
 #5971
 
-def next(i,root,overshoot):
+def endtree(i,root,overshoot):
 	head=df.iloc[i]['head'].split('-')[0]
 	#input('?')
-	print(i,head,root)
+	#print(i,head,root)
 	if i > overshoot:
-		print(i,head,root)
 		print('overshot ',overshoot, i,root,head)
-		raise Error 
-		return -1
-	if root==None:
-		return -1
-	if head!=root:
-		return i+1
-	else:
-		return -1
+		raise ValueError 
+	if root==None or head==root:
+		return True
+	return False
+
+
+def findend(h,i):
+	idx=df[df['head'].str.contains(h,na=False)].index.to_list()
+	return idx[idx.index(i)+1]
+
+
 #df[df['head'].str.startswith('77777',na=False)].index
 #traverse list make tree
 #"if node has children" was an test in json traversal 
 #"is node child" is test in list trav
-def genTree(i, overshoot,dict_parent={}, root=None):
+def genTree(i, overshoot, root=None,subgroup=False):
+	parent_tree={}
 	while True:
 		try:
 			head=df.iloc[i]['head'].split('-')[0]
 		except AttributeError as e:
 			print(i, df.iloc[i]['head'])
 			raise e
+		#print('p ',i, head,root,subgroup, df.iloc[i]['desc'])
 		data=df.iloc[i][['2018', '2019Est', '2019Rev', '2020Est']]
-		#if node is parent
-		if data.isnull().all():
-			#this is parent[head][head]
-			dict_parent[head]={'desc':df.iloc[i]['desc']}
-			#print(dict_parent)
-			i,t=genTree(i+1,overshoot,dict_parent[head],head)
-			dict_parent[head].update(t)
+		desc=df.iloc[i]['desc']
+		dpcode=df.iloc[i]['dpcode']
+		#node is a header
+		#if data.isnull().all(): < this fails if single empty space exists
+		if data.apply(lambda x:True if p.isnull(x) or x==' ' else False).all():
+			#this is parent[head][head]			
+			parent_tree[head]={'desc':desc}
+			end=findend(head, i)
+			i,t=genTree(i+1, end,head)#parent_tree[head]
+			parent_tree[head].update(t)
+		# group detected
+		elif not p.isna(dpcode) and dpcode.endswith('00'):
+			if subgroup: # this indicates end of prev subgroup
+				print('new sub found but prev subgrp needs to be added')
+				return i-1,parent_tree
+			print('grouping',head)
+			parent_tree[head]={'desc':desc}
+			parent_tree[head].update(data.to_dict())
+			i,t=genTree(i+1,overshoot,head,subgroup=True)
+			parent_tree[head].update(t)
+			#pprint(dict_parent)
+			print('ungrouped')
+			#return i,parent_tree
+		# end of group total
+		elif p.isna(dpcode) and desc.startswith('Total'):
+			if subgroup:
+				print('total found so prev sub needs to be added')
+				return i-1,parent_tree
+			#parent_tree[head]={'desc':desc}
+			parent_tree[desc]=data.to_dict()
 		else:
 			#node is child
-			dict_parent[head]={'desc':df.iloc[i]['desc']}
-			dict_parent[head].update(data.to_dict())
+			print('adding '+head)
+			parent_tree[head]={'desc':desc}
+			parent_tree[head].update(data.to_dict())
 		#pprint(dict_parent)
-		
-		if next(i,root,overshoot) == -1:
+		if endtree(i,root,overshoot):
 			break;
 		else:
 			i=i+1
-	return i,dict_parent
+	return i,parent_tree
+
+def dp2node(head,desc,data,dpcode):
+	#1601 06 101 DG 13105
+	mjr,sub,mnr,grp,subg=dpcode.split(' ')
+	#subg[0],subg[1:3],sub[3:5]
+	g1=subg[0:3]
+	g2=subg[3:5]
+	n={mjr:{sub:{mnr:{grp:{}}}}}
+
+def df2json(start):
+	root=df.iloc[start]['head'].split('-')[0]
+	tmp=[]
+	output={}
+	while True:
+		try:
+			head=df.iloc[start]['head'].split('-')[0]
+		except AttributeError as e:
+			print(i, df.iloc[i]['head'])
+			raise e
+		desc=df.iloc[i]['desc']
+		data=df.iloc[i][['2018', '2019Est', '2019Rev', '2020Est']]
+		dpcode=df.iloc[i]['dpcode']
+		#if node is just header
+		if data.isnull().all() and p.isna(dpcode):
+			tmp.append(head,desc)
+		else:
+			dp2node(head,desc,data,dpcode)
+
+
+
 
 def dict_generator(indict, pre=None):
     pre = pre[:] if pre else []
@@ -89,15 +146,14 @@ def walk(node,parent=None,level=0):
 				print('CHECK ',key)
 
 from pprint import pprint
-walk(h)
+#walk(h)
+def dump(name,data):
+	f=open(name,'w')
+	tmp=json.dumps(data)
+	json.dump(json.loads(tmp.replace('NaN','""')),f)
+	f.close()
 
 
-# i=3685
-# l=genTree(i)
-# f=open('dairy.csv','w')
-# tmp=json.dumps(l[1])
-# tmp.replace('NaN','')
-# json.dump(l[1],f)
-# f.close()
-
+l=genTree(3687,3721)
+dump('dairy.json',l[1])
 
