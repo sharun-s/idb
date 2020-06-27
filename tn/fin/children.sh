fc10523be3b62f58b33f3b267334a31a4ebf74a4
sharun <sharun.msgs@gmail.com>
2020-06-27 22:48:13 +0530

    regen of sub head json files pulling in correction from rev_details.json

---
+++ tn/fin/children.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+# can be 2019Est,2019Rev,2020Est
+year="${2:-2018}" 
+echo "$1"
+cat revrec-summary.json | jq --arg code "$1" '..| to_entries? |.[]| select(.value.code? ==$code) | .key '
+cat revrec-summary.json | jq -r --arg code "$1" --arg year "$year" '..|select(.code? == $code )|to_entries[]| [(.key), (.value|.[$year]?)]|map(.)|@csv'
