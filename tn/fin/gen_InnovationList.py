a0db2504fba2594525b826280c07558a25328384
sharun <sharun.msgs@gmail.com>
2020-08-07 03:25:15 +0530

    auto add amts to sif tables during details page gen

--- tn/fin/gen_InnovationList.py
+++ tn/fin/gen_InnovationList.py
@@ -2,8 +2,21 @@ import sys,re
 import pandas as p
 import subprocess
 import csv
-
 p.set_option('display.max_colwidth', -1)
+
+def get_amounts(filename,dpcode):
+	g=p.read_csv(filename)
+	tmp=g[g['dpcode']==dpcode].index[0]
+	head=g.ix[tmp]['head']
+	idx=tmp
+	while True:
+		idx=idx+1
+		if g.ix[idx]['head']==head:
+			if g.ix[idx]['desc']=='Total '+head:
+				return g.ix[idx][['2018','2019Rev','2020Est']]
+		if idx>tmp+20:
+			return ['?','?','?']
+
 try:
 	subprocess.run(r'rm sif_index.html',shell=True)
 	subprocess.run(r'rm sif_explorer/*',shell=True)
@@ -22,8 +35,12 @@ for i in csv.reader(y):
 	dept=filename.split("-")[0]
 	subdeptname=re.sub(r'([0-9]{1,2}[\-_])+','',filename)
 	functional_head=i[6].split(' ')[0]
-	d.append([i[1].replace('under State Innovation Fund',''),subdeptname.replace('_',' '),dept,functional_head])
-df=p.DataFrame(d,columns=['Project','SubDept','Dept','Functional Head'])
+	row=[i[1].replace('under State Innovation Fund',''),subdeptname.replace('_',' '),dept,functional_head]
+	row.extend(get_amounts(i[0].split('.')[0]+'.csv', i[-1]))
+	#assert len(row)==7
+	d.append(row)
+
+df=p.DataFrame(d,columns=['Project','SubDept','Dept','Functional Head','2018','2019','2020'])
 
 with open('sif_index.html','a') as f:
 	f.write('<body style="font-family:verdana,sans-serif;">')
@@ -32,6 +49,7 @@ dept_map=p.read_csv('tn_dept2subdept_map',header=None)
 
 vc=df.Dept.value_counts()
 l=[]
+#gets all rows for a each dept, sorted by dept with most projects, convert deptcode to name
 for i in vc.index:
 	tmp=df[df['Dept'] == i]
 	d=dept_map[dept_map[0] == int(i)].iloc[0][2]
@@ -39,16 +57,16 @@ for i in vc.index:
 	fd=d.strip().replace(' ','_').replace('_DEPARTMENT','').title()
 	cnt=vc.ix[i]
 	l.append([fd,tmp,txt,cnt])
+# resort dept alphabetically, easier to browse than abv order
 for fd,tmp,txt,cnt in sorted(l,key=lambda x:x[2]):
 	with open(f'sif_explorer/{fd}.html','w') as f:
 		f.write('<body style="font-family:verdana,sans-serif;">')
 		f.write('<div><a href=../startpage.html target=details>Go Back</a></div>')
-		f.write(tmp.to_html(index=False,columns=['Project','SubDept','Functional Head']))
+		f.write(tmp.to_html(index=False,justify='center',na_rep='',columns=['Project','2018','2019','2020','SubDept','Functional Head']))
 		f.write('</body>')
 	with open('sif_index.html','a') as f:
 		#f.write('<body style="font-family:verdana,sans-serif;">')
 		f.write(f'<div><a href=sif_explorer/{fd}.html target=details>{txt}</a>&nbsp;{cnt}</div>')
-
 
 #index by subdept
 # for i in df.SubDept.unique():
