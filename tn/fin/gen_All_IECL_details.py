437d7b19ad85437c0864c8c5b1aa9e9c408f6c3a
sharun <sharun.msgs@gmail.com>
2020-08-08 05:24:32 +0530

    changes to gen_ALL_IECL_details.py to produce sorted indexes

--- tn/fin/gen_All_IECL_details.py
+++ tn/fin/gen_All_IECL_details.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # generates files in loan_explorer, investment_explorer, expense_explorer, income_explorer
 # for income json breakup details file is parsed for sub head [A-Z][A-Z]
-# for revex,capex,loan simple grep of the functional head across all demand files
-# generates indexes - loan_index capex_index revex_index 
-
+# for revex,capex,loan simple grep of the functional head across all demand files TODO get amts pull code from dept-summary2html
+# generates indexes - loan_index capex_index revex_index income_index
 
 import sys
 import pandas as p
@@ -50,7 +49,7 @@ def format_indian(t):
 	m=fman(t)
 	return "{:.2f}".format(m*dic[ex][1])+" "+dic[ex][0]
 
-def ppIncomeByDepts(detailsfile,head):
+def dumpIncomeDetails(detailsfile,head):
 	#Find all sub depts generating income
 	o=subprocess.run(r"cat "+ shlex.quote(detailsfile.strip()) + ' | jq -r \'paths as $p|getpath($p) | select(scalars)| ($p | map(select(type=="string"))) + [.]|@csv\'' + r"| grep '\[.*\]\",\"Total'  | grep 2018 " ,shell=True, stdout=subprocess.PIPE, universal_newlines=True)
 	rows=[]
@@ -62,6 +61,7 @@ def ppIncomeByDepts(detailsfile,head):
 				break
 		rows.append([i[descidx],i[descidx+3]]) 
 	df=p.DataFrame(rows)
+	#get the dept subdept code generating the revenue
 	try:
 		df[[0,2]]=df[0].str.split('[',expand=True)
 		df[2]=df[2].str.replace(']','')
@@ -70,7 +70,7 @@ def ppIncomeByDepts(detailsfile,head):
 		raise e
 	
 	m={}
-	#print(df[2].unique())
+	# using the code get the names of the dept and subdept
 	for dept in df[2].unique():
 		#print(detailsfile,head,dept)
 		d=dept_map[dept_map[0] == int(dept[:2])]			
@@ -84,13 +84,21 @@ def ppIncomeByDepts(detailsfile,head):
 			print(d)
 			raise e
 		
-	df.columns=['head','amt','code']
-	df['subdept']=df['code'].apply(lambda x:m[x])
+	df.columns=['Head','Amt','Code']
+	df['subdept']=df['Code'].apply(lambda x:m[x])
 	with open(f'income_explorer/{head}.html','w') as f:
 		f.write('<body style="font-family:verdana,sans-serif;">')
-		f.write(f'<br>--Income Sources (Revenue)<br>')
-		f.write(df.to_html(index=False,border=0,columns=['head','amt','subdept']))
+		f.write(f'<a href=../startpage.html target=details>Home</a>&nbsp;<br>--Income Sources (Revenue)<br>')
+		df.style.set_table_styles([dict(selector="th",props=[("text-align", "center")])])
+		f.write(df.to_html(index=False,border=0,justify='center',columns=['Head','Amt','Code']))
 		f.write('</body>')	
+	try:
+		r=func_map[func_map[0]==head].iloc[0]
+		return (r[0],r[1],'') # TODO: get the total the way dept-summary2html does
+	except Exception as e:
+		print(head)
+		raise e
+    
 
 def parseResults(o):
 	d=[]
@@ -132,7 +140,7 @@ def parseResults(o):
 def highlight(x):
     return ['font-weight: bold' for v in x]
 
-def pp(d,v,ftitle,titleStr,detailsdir):		
+def dumpDetails(d,v,ftitle,titleStr,detailsdir):		
 	dk=p.DataFrame(d)#v,index=[i.title() for i in d])
 	if(len(dk)==0):
 		return False
@@ -140,50 +148,68 @@ def pp(d,v,ftitle,titleStr,detailsdir):
 
 	with open(f'{detailsdir}/{ftitle}.html','w') as f:
 		f.write('<body style="font-family:verdana,sans-serif;">')
-		f.write(f'<b>--{titleStr}</b><br>')
+		f.write(f'<a href=../startpage.html target=details>Home</a>&nbsp;<b>--{titleStr}</b><br>')
 		f.write(dk.to_html(header=False,index=False,border=0,bold_rows=False,na_rep='-'))
 		f.write('<br><a href="history_explorer/dummy.html">[Historic Data 2002-2018]</a>')
 		f.write('</body>')
 		f.write('\n')
 	return True
+		
+def writeIndex(index, indexfile,detailsdir):
+	#remove None if any exist in index
+	index=[i for i in index if i]
+	with open(indexfile,'w') as f:
+		f.write('<body style="font-family:sans-serif;">')
+		f.write('<div>Sort by: <a target=ind href="#">Amount</a>&nbsp;&nbsp;<a target=ind href="#">A-Z</a>&nbsp;&nbsp;<a target=ind href="#">Code</a></div>')
+		for detailsfile,funcname,totamt in sorted(index,key=lambda x:x[1]):
+			# format total if its a float
+			if isinstance(totamt,float):
+				f.write(f'<div style="font-family:sans-serif;"><a href="{detailsdir}/{detailsfile}.html" title="{format_indian(1000*totamt)}" target=details>{funcname.title()}</a></div>')
+			else:
+				f.write(f'<div style="font-family:sans-serif;"><a href="{detailsdir}/{detailsfile}.html" title="{totamt}" target=details>{funcname.title()}</a></div>')
+		f.write('</body>')
 
-def updateIndex(head,ta,indexfile,detailsdir):
-	#updateincomeindex=False
-	with open(indexfile,'a') as f:
-		try:
-			r=func_map[func_map[0]==head].iloc[0]
-		except Exception as e:
-			print(head)
-			raise e
-		f.write(f'<div style="font-family:sans-serif;"><a href="{detailsdir}/{r[0]}.html" title="{ta}" target=details>{r[1].title()}</a></div>')
-
-def SubDeptsBreakup(titleStr, head, indexfile, detailsdir):
+def SubDeptsBreakup(titleStr, head,detailsdir):
 	#note this catches head at start of line only
 	#o=subprocess.run(r"grep -Ir '^"+head+"' data/expenditure --exclude-dir=tmp",shell=True, stdout=subprocess.PIPE, universal_newlines=True)	
 	#this catches head anywhere - esp all occurrences within dpcode
 	o=subprocess.run(r"grep -Ir '"+head+"' data/expenditure --exclude-dir=tmp",shell=True, stdout=subprocess.PIPE, universal_newlines=True)	
 	d,v,ta=parseResults(o)
-	if pp(d,v,head,titleStr,detailsdir):
-		updateIndex(head,format_indian(ta*1000),indexfile,detailsdir)
+	if dumpDetails(d,v,head,titleStr,detailsdir):
+		try:
+			r=func_map[func_map[0]==head].iloc[0]
+			return (r[0],r[1],ta)
+		except Exception as e:
+			print(head)
+			raise e
 		#print('done',head)
+	#NOTE no else so None gets returned in some cases and is removed from index in writeIndex
 	#print(f'<b>{format_indian(ta*1000)}</b>')
 
 dept_map=p.read_csv('tn_dept2subdept_map',header=None)
 #skipping error lines cause just interested in code to major head map, 3 col rows not reqd
 func_map=p.read_csv('tn_func2dept_map',dtype=str,header=None,error_bad_lines=False,warn_bad_lines=False)
+revex_index=[]
+capex_index=[]
+loan_index=[]
+rev_index=[]
 
 for head in func_map[0]:
 	if head[0]=='2' or head[0]=='3':
-		SubDeptsBreakup("Expenditure day to day (revex)", head,'revex_index.html','expense_explorer')
+		revex_index.append(SubDeptsBreakup("Expenditure day to day (revex)", head, 'expense_explorer'))
 	elif head[0]=='4' or head[0]=='5':
-		SubDeptsBreakup('Investments (capex)',head,'capex_index.html','investment_explorer')
+		capex_index.append(SubDeptsBreakup('Investments (capex)',head,'investment_explorer'))
 	elif head[0]=='6' or head[0]=='7':
-		SubDeptsBreakup('Loans',head,'loan_index.html','loan_explorer')
+		loan_index.append(SubDeptsBreakup('Loans',head,'loan_explorer'))
 	else:
 		pass#print('Unrecognized head',head)
 
+writeIndex(revex_index,'revex_index.html','expense_explorer')
+writeIndex(capex_index,'capex_index.html','investment_explorer')
+writeIndex(loan_index,'loan_index.html','loan_explorer')
+
 revfiles=listdir('data/revenue/breakup/')
 for f in revfiles:
-	ppIncomeByDepts(f'data/revenue/breakup/{f}', f[:4])
-	updateIndex(f[:4],'','income_index.html','income_explorer')
-	print('done',f)
+	rev_index.append(dumpIncomeDetails(f'data/revenue/breakup/{f}', f[:4]))
+writeIndex(rev_index,'income_index.html','income_explorer')
+#print('done',f)
