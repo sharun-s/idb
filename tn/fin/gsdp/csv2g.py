d881c585ec214f9b49dd66a7155ecf1d0fe50ac8
sharun <sharun.msgs@gmail.com>
2020-09-20 10:52:11 +0530

    refactor

--- tn/fin/gsdp/csv2g.py
+++ tn/fin/gsdp/csv2g.py
@@ -5,7 +5,7 @@ import common
 #NB: tmp hack because of import common run from parent dir - idb/tn/fin$python3 -m gsdp.csv2g
 
 def newfig(title):
-	fig = plt.figure(facecolor="#001f3f")#,figsize=(8.,6.4))
+	fig = plt.figure(facecolor="#001f3f",figsize=(7.2,7.2))
 	fig.suptitle(title, color="#00efde", fontsize=16)
 	ax = fig.add_subplot(111, frameon=False)
 	ax.set_facecolor("#002f4f")
@@ -18,6 +18,7 @@ def newfig(title):
 	ax.tick_params(axis='x', colors='#E6DB74')#
 	return fig,ax
 
+# color inner and outer labels differently
 def cleanup_pie(ax):
 	for t in ax.texts:
 		if t.get_text().find('%') > -1:
@@ -29,35 +30,66 @@ def cleanup_pie(ax):
 
 
 df=p.read_csv('gsdp/data/TamilNadu_GSDP_27-08-2020.csv',comment="#")
-Sectors=df[df['Head'].isna()].iloc[:,2:].set_index('Desc').T
-SubSectors=df[df['Subhead'].isna()].iloc[:14,2:].set_index('Desc').T
-SubSectors.columns=['Agriculture, forestry, fishing', 'Mining','Primary',
-       'Manufacturing','Electricity, gas, water supply & utility','Construction','Secondary', 
-       'Trade, repair, hotels','Transport, storage, broadcasting','Finance','Real estate','Public administration', 'Other','Tertiary'
-       ]
+SectorsByYear=df[df['Head'].isna()].iloc[:,2:].set_index('Desc').T
+SubSectorsByYear=df[df['Subhead'].isna()].iloc[:14,2:].set_index('Desc').T
+SubSectorsByYear.drop(['Primary','Secondary','Tertiary'],axis=1,inplace=True)
+#rename cols to make labels smaller
+SubSectorsByYear.columns=['Agriculture Forestry Fishing', 'Mining',#'Primary',
+    'Manufacturing','Electricity Gas Water & utility','Construction',#'Secondary', 
+    'Trade Repair Hotels','Transport Storage Broadcasting','Finance','Real Estate','Public Administration', 'Other'#,'Tertiary'
+]
 
-for i in Sectors.index:
-	fig,ax = newfig('TN GDP ' + str(i))
-	q=[ k +'\n'+common.format_indian(j*100000.0) for k,j in Sectors.loc[i].items() ]
-	tot=common.format_indian(sum(i*100000.0 for i in Sectors.loc[i].values))
-	w=Sectors.loc[i].plot.pie(labels=q,autopct='%1.1f%%',wedgeprops=dict(width=0.6))
-	#w=Sectors.loc[i].plot(kind='pie',autopct='%1.1f%%',radius=.7,wedgeprops=dict(width=0.3, edgecolor='w'))
-	cleanup_pie(w)
-	tax=common.format_indian(100000.0*df.iloc[28][i])
-	subsidy=common.format_indian(100000.0*df.iloc[29][i])
-	gsdp=common.format_indian(100000.0*df.iloc[30][i])
-	w.annotate('GVA', xytext=(.05,.5), xycoords='figure fraction', xy=(.45,.51), 
-		arrowprops={'arrowstyle':'<-','ec':'w'}, 
-		color='white')
-	w.annotate('+(Tax) '+tax, xycoords='figure fraction', xy=(.051,.4), color='white')
-	w.annotate('-(Subsidy) '+subsidy, xycoords='figure fraction', xy=(.051,.3), color='white')
-	w.annotate('= '+gsdp + '\n(State GDP)', xycoords='figure fraction', xy=(.051,.17), fontsize=12, fontweight='heavy', 
-		color='#00effe')
-	w.annotate(tot, xycoords='figure fraction', xy=(.45,.5), fontweight='bold', color='white')
-	# cols = ['Agriculture, forestry, fishing', 'Mining',
- #       'Manufacturing','Electricity, gas, water supply & utility','Construction', 
- #       'Trade, repair, hotels','Transport, storage, broadcasting','Finance','Real estate','Public administration', 'Other']
-	# w=SubSectors.loc[i][cols].plot(kind='pie',radius=1,wedgeprops=dict(width=0.3, edgecolor='w'))
-	# cleanup_pie(w)
+#cmap = plt.get_cmap("tab20")
+outer_colors = ["#00d0ff","#ffc107",'#00ff88']#,'#AE81FF','#A6E22E','#F92672'
+inner_colors = ["#00d0cc","#00d0aa",
+				"#aac122","#aac144","#aac188",
+				"#22aa88",'#44aa88',"#66aa88",'#88aa88',"#88aa44",'#bbaa22'
+				]
+
+def addGDPCalculationText(ax, year):
+	tot=common.format_indian(100000.0*df.iloc[27][year])
+	tax=common.format_indian(100000.0*df.iloc[28][year])
+	subsidy=common.format_indian(100000.0*df.iloc[29][year])
+	gsdp=common.format_indian(100000.0*df.iloc[30][year])
 	
-	fig.savefig('gsdp/s-'+i+'.png',format='png',facecolor=fig.get_facecolor())
+	ax.plot([.5,.5],[1,1],".")
+	ax.annotate('GVA', xytext=(.05,.9), xycoords='figure fraction', xy=(.45,.51), 
+		arrowprops={'arrowstyle':"<-",'ec':'w','connectionstyle':"angle,angleA=-90,angleB=180,rad=0"}, 
+		color='white')
+	ax.annotate('+(Tax) '+tax, xycoords='figure fraction', xy=(.12,.9), color='white')
+	ax.annotate('-(Subsidy) '+subsidy, xycoords='figure fraction', xy=(.33,.9), color='white')
+	ax.annotate('= '+gsdp + ' (State GDP)', xycoords='figure fraction', xy=(.56,.9), fontsize=12, fontweight='heavy', 
+		color='white')
+	ax.annotate(tot + '\n\n'+ year, xycoords='figure fraction', xy=(.45,.48), fontweight='bold', 
+		color='white')
+
+def formatData(data):
+	details={}
+	for i in data.index:
+		labelandamt=[ k +'\n'+common.format_indian(j*100000.0) for k,j in data.loc[i].items() ]
+		#tot=common.format_indian(sum(i*100000.0 for i in data.loc[i].values))
+		details[i]=labelandamt
+	return details
+
+details=formatData(SectorsByYear)
+subdetails=formatData(SubSectorsByYear)
+
+years=SectorsByYear.index
+
+for year in years:
+	fig,ax = newfig('Tamil Nadu GDP')
+	w=SectorsByYear.loc[year].plot.pie(labels=details[year],#autopct='%1.1f%%'
+		radius=.55, 
+		colors=outer_colors, wedgeprops=dict(width=0.2))
+	#cleanup_pie(w)
+
+	w=SubSectorsByYear.loc[year].plot.pie(labels=subdetails[year],radius=1, colors=inner_colors,
+		wedgeprops=dict(width=0.1, edgecolor='white'))
+	cleanup_pie(w)
+
+	addGDPCalculationText(w, year)
+	plt.annotate('Data:http://mospi.nic.in/GSVA-NSVA', (0.,0), (0, -25), 
+                xycoords='axes fraction', textcoords='offset points', 
+                color='#E6DBff', va='top', fontstyle='italic')
+
+	fig.savefig('gsdp/s-'+year+'.png',format='png',facecolor=fig.get_facecolor())
