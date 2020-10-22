01b1318b168c6edd29aa82ff00bb0e880a263990
sharun <sharun.msgs@gmail.com>
2020-10-22 14:37:47 +0530

    arg parser added to getwholetabl

--- tn/fin/getwholetabl.py
+++ tn/fin/getwholetabl.py
@@ -4,20 +4,23 @@ from locale import atof
 locale.setlocale(locale.LC_NUMERIC, '')
 import pandas as p
 import numpy as np
+import argparse
+parser = argparse.ArgumentParser(description='Basic Table extract from pdf')
+parser.add_argument('infile', help='input pdf filename')
+parser.add_argument('pages', help='page number eg:2 3-19 (camelot format)')
+parser.add_argument('startrow', type=int, help='int usually passed to skip header rows')
+parser.add_argument('-endrow', default=-1,type=int, help='int passed to skip footerrows. Default is -1 ie last row')
+parser.add_argument('--cols',default="1,2", help='columns comma seperated')
+parser.add_argument('-tableno', type=int, default=0, help='table number if more than 1 table in page. Default 0')
+parser.add_argument('outfile', help='output filename specify extn')
 
-fname=sys.argv[1]
-pg=sys.argv[2]
-startrow=int(sys.argv[3]) 
-outfile=sys.argv[4]
-endrow=-1 if len(sys.argv) <=5 else int(sys.argv[5])
-cols=[2,4] if len(sys.argv) <=6 else [int(i) for i in sys.argv[6].split(',')]
+args = parser.parse_args()
+print(args)
+cols=[int(i) for i in args.cols.split(',')]
 
-print(startrow,endrow,cols)
-
-
-tables=camelot.read_pdf(fname,pages=pg)
+tables=camelot.read_pdf(args.infile,pages=args.pages)
 # multipage requires merge
-if pg.find('-') > 0:
+if args.pages:#pg.find('-') > 0:
 	merge=[]
 	for i in range(0,len(tables)):
 		#merge.append(tables[i].df[[2,4]][startrow:].applymap(lambda x:atof(x) if x else None))
@@ -31,11 +34,11 @@ if pg.find('-') > 0:
 					print(e)
 					continue
 				
-		merge.append(tables[i].df[cols][startrow:])#.applymap(lambda x:atof(x) if x else None))
+		merge.append(tables[i].df[cols][args.startrow:])#.applymap(lambda x:atof(x) if x else None))
 	merged=p.concat(merge,axis=1)
-	merged.to_csv(sys.argv[4]+'.csv',index=False,header=False)
+	merged.to_csv(args.outfile,index=False,header=False)
 else:
-	tno=int(sys.argv[7]) # 0 - top table 1 bottom tabl
-	result=tables[tno].df[cols][startrow:endrow].applymap(lambda x:atof(x) if x else None)
+	#tno=int(args.tno) # 0 - top table 1 bottom tabl
+	result=tables[args.tno].df[cols][args.startrow:args.endrow].applymap(lambda x:atof(x) if x else None)
 	print(result)
-	p.DataFrame(result).to_csv(outfile,mode='a',header=False,index=False)
+	p.DataFrame(result).to_csv(args.outfile,mode='a',header=False,index=False)
