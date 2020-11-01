d9c1da99795367face152f73191243ac9d19569a
sharun <sharun.msgs@gmail.com>
2020-11-01 19:03:56 +0530

    added dumpall to district deposit/credit grapher

--- tn/fin/district_banking/csv2g.py
+++ tn/fin/district_banking/csv2g.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt;import pandas as p
-import argparse
+import argparse,sys
 import common
 from locale import atof
 from matplotlib.colors import Normalize
@@ -36,7 +36,7 @@ def formatData(data):
 parser = argparse.ArgumentParser(description='Total Deposits Grapher')
 parser.add_argument('-csv', default="district_banking/districtwise_deposits_credit.csv",help='input csv')
 parser.add_argument('-pie', default=False, help='show only latest quarter districtwise', action='store_true')
-#parser.add_argument('-line',action="store_true", help='show all quarter [default]')
+parser.add_argument('-dumpall',action="store_true", help='show all data dump all districts')
 parser.add_argument('--outfile', help='output filename specify extn')
 
 args = parser.parse_args()
@@ -55,20 +55,26 @@ disam={'tiruppur':'West',
 'pudukkottai':'East',
 'nilgiris':'West'} 
 
-fig,ax=newfig('TN Districts - Deposits in Scheduled Banks - 2020')
-
-d=p.read_csv(args.csv,skiprows=2)
+alldata=p.read_csv(args.csv,skiprows=2)
 
 region=p.read_csv('gsdp/data/ddp-currentprices.csv',skiprows=2)
 rmap=region[['District','Region']]
 
-depcol=[i for i in d.columns if i.find('Deposit') > -1]
-t=d[depcol].T
-t.columns=d['District']
-tot=common.format_indian(10000000.0*t.loc['2020-21:Q1 Deposit']['Total'])
+depcol=[i for i in alldata.columns if i.find('Deposit') > -1]
+credcol=[i for i in alldata.columns if i.find('Credit') > -1]
+branchcol=[i for i in alldata.columns if i.find('Offices') > -1]
+
+d=alldata[depcol].T
+d.columns=alldata['District']
+c=alldata[credcol].T
+c.columns=alldata['District']
+b=alldata[branchcol].T
+b.columns=alldata['District']
+
+tot=common.format_indian(10000000.0*d.loc['2020-21:Q1 Deposit']['Total'])
 print(tot)
 #drop state total so it doesn't get plotted
-t.drop('Total',axis=1,inplace=True)
+d.drop('Total',axis=1,inplace=True)
 
 def get_colors(data):
 	colors=[]
@@ -82,7 +88,9 @@ def get_colors(data):
 	return colors
 
 if args.pie:
-	latest=t.loc['2020-21:Q1 Deposit'].T.sort_values(ascending=False)
+	fig,ax=newfig('TN Districts - Deposits in Scheduled Banks - 2020')
+
+	latest=d.loc['2020-21:Q1 Deposit'].T.sort_values(ascending=False)
 	latestlabels=formatData(latest)
 	latest.plot(kind='pie',ax=ax,autopct='%1.1f%%',
 		labels=latestlabels, #cmap=plt.get_cmap("Paired"),
@@ -121,8 +129,31 @@ if args.pie:
 	ax.annotate('TN Total \n\n'+ tot , xycoords='figure fraction', xy=(.45,.48), fontweight='bold', 
 		color='white')
 else:
-	t.plot(ax=ax)
-	ax.invert_xaxis()
+	if args.dumpall:
+		for i in d.columns:
+			fig,a=newfig(i+' - Deposits/Credit 2020')
+			d[i].plot(ax=a, label="Deposits", legend=True, color='limegreen')
+			c[i].plot(ax=a, label="Credit", legend=True, color='red')
+			a.invert_xaxis();
+			a.set_ylabel('CRORES',color="#d6cB74")
+			#print(dir(a.get_xticklabels()[2]))
+			a.fill_between(range(0,13),d[i].values,c[i].values,where=d[i].values > c[i].values, facecolor='green', alpha=0.7)
+			a.fill_between(range(0,13),d[i].values,c[i].values,where=d[i].values < c[i].values, facecolor='salmon', alpha=0.4)
+			
+			a.set_xticklabels([xt.get_text().split(' ')[0] for xt in a.get_xticklabels() if xt])
+			fig.savefig('deposits_credit_2017-2020'+i+'.png',format='png',facecolor=fig.get_facecolor())
+	else:
+		fig,ax=newfig('TN Districts - Credit - 2020')
+		c.plot(ax=ax,legend=False)
+		ax.set_yscale('log')
+		ax.invert_xaxis()
+		for line in ax.lines:
+			y = line.get_ydata()[0]
+			ax.annotate(line.get_label().title(), xy=(1,y), xytext=(0,0), color='#d6cB74',#line.get_color(), 
+        		xycoords = ax.get_yaxis_transform(), 
+        		textcoords="offset points",
+        		size=8)
+
 
 plt.annotate('Data:rbi.org.in (Quarterly Deposit Stats)', (0.,0), (0, -25), 
                 xycoords='axes fraction', textcoords='offset points', 
@@ -130,5 +161,7 @@ plt.annotate('Data:rbi.org.in (Quarterly Deposit Stats)', (0.,0), (0, -25),
 
 if args.outfile:
 	fig.savefig(args.outfile,format='png',facecolor=fig.get_facecolor())
+elif args.dumpall:
+	pass
 else:
 	plt.show()
